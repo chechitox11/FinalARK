b1f15d5def18abedfadc1224f319fcff903fe40a
chechitox11 <sergio_a_bob@hotmail.com>
2021-09-01 16:50:49 -0500

    primer commit

---
+++ src/bb/Distancia.java
@@ -0,0 +1,14 @@
+package bb;
+
+public class Distancia 
+{
+	int x;
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+}
---
+++ src/grafo/Grafo.java
@@ -0,0 +1,16 @@
+package grafo;
+
+import java.util.Vector;
+
+public class Grafo {
+ 
+ private String dato;
+ 
+ public Grafo(String s){
+  dato=s;
+ }
+ 
+ public String obtenerDato(){
+  return dato;
+ }
+}
